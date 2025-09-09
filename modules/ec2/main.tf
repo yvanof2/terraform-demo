417167720a7f853fbcc7201837dc87db84114235
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 01:30:31 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -13,3 +13,23 @@ resource "aws_instance" "this" {
 }
 
 # Output EC2 instance ID
+# EC2 Security Group
+resource "aws_security_group" "ec2" {
+  name        = "ec2_sg"
+  description = "Security group for EC2 instance"
+  vpc_id      = var.vpc_id  # Pass vpc_id from module input
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
