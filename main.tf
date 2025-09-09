384f5d76f7efbbaa60f284122f3128c2393b91b0
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 00:40:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,14 +12,43 @@ module "network" {
 }
 
 # ----------------------
+# EC2 Security Group
+# ----------------------
+resource "aws_security_group" "ec2" {
+  name   = "ec2_sg"
+  vpc_id = module.network.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ----------------------
 # EC2 Module
 # ----------------------
 module "ec2" {
-  source        = "./modules/ec2"
-  ami           = var.ec2_ami
-  instance_type = var.ec2_instance_type
-  subnet_id     = module.network.public_subnet_id
-
+  source           = "./modules/ec2"
+  ami              = var.ec2_ami
+  instance_type    = var.ec2_instance_type
+  subnet_id        = module.network.public_subnet_id
+  security_group_id = aws_security_group.ec2.id
   user_data = <<-EOF
               #!/bin/bash
               echo "Welcome to Bido's demo" > /var/www/html/index.html
@@ -30,12 +59,13 @@ module "ec2" {
 # RDS Module
 # ----------------------
 module "rds" {
-  source        = "./modules/rds"
-  subnet_id     = module.network.private_subnet_id
-  db_name       = var.db_name
-  username      = var.db_username
-  password      = var.db_password
-  instance_type = var.db_instance_type
+  source            = "./modules/rds"
+  subnet_id         = module.network.private_subnet_id
+  db_name           = var.db_name
+  username          = var.db_username
+  password          = var.db_password
+  instance_type     = var.db_instance_type
+  security_group_id = module.network.rds_sg_id
 }
 
 # ----------------------
@@ -46,4 +76,19 @@ module "alb" {
   vpc_id    = module.network.vpc_id
   subnet_id = module.network.public_subnet_id
   target_id = module.ec2.instance_id
+}
+
+# ----------------------
+# Outputs
+# ----------------------
+output "alb_url" {
+  value = module.alb.alb_dns_name
+}
+
+output "ec2_instance_id" {
+  value = module.ec2.instance_id
+}
+
+output "rds_endpoint" {
+  value = module.rds.db_endpoint
 }
