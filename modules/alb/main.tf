dd75b6e23d60aaaaf367f66ca8fa9685bc8bdd88
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 01:24:35 -0400

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,19 +1,16 @@
-variable "subnet_id" {}
-variable "security_group_id" {}
-
 resource "aws_lb" "this" {
   name               = "demo-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [var.subnet_id]       # Only one subnet (1 AZ)
+  subnets            = [var.subnet_id]        # 1 AZ
   security_groups    = [var.security_group_id]
 }
 
 resource "aws_lb_target_group" "this" {
-  name     = "demo-tg"
-  port     = 80
+  name   = "demo-tg"
+  port   = 80
   protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  vpc_id = var.vpc_id
 }
 
 resource "aws_lb_listener" "this" {
