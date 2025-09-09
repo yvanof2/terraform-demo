925c1c4a23c981f95accfe715e6b6dd1ea891a41
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 01:19:58 -0400

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,27 +1,28 @@
-resource "aws_elb" "this" {
-  name               = "demo-elb"
-  subnets            = [var.subnet_id]
+variable "subnet_id" {}
+variable "security_group_id" {}
+
+resource "aws_lb" "this" {
+  name               = "demo-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [var.subnet_id]       # Only one subnet (1 AZ)
   security_groups    = [var.security_group_id]
-  cross_zone_load_balancing = true
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "HTTP"
-    lb_port           = 80
-    lb_protocol       = "HTTP"
-  }
-
-  health_check {
-    target              = "HTTP:80/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-
-  instances = var.instance_ids
 }
 
-output "elb_dns_name" {
-  value = aws_elb.this.dns_name
+resource "aws_lb_target_group" "this" {
+  name     = "demo-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
 }
