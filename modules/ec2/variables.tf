24ab2ae55694ed0c803f240197a995800ca41113
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 00:48:13 -0400

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,16 +1,25 @@
-variable "subnet_id" {
-  type = string
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
 }
 
 variable "instance_type" {
-  type = string
+  description = "EC2 instance type"
+  type        = string
 }
 
-variable "ami" {
-  type = string
+variable "subnet_id" {
+  description = "Subnet ID where EC2 will be deployed"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID for the EC2 instance"
+  type        = string
 }
 
 variable "user_data" {
-  type = string
-  default = ""
+  description = "User data script for the EC2 instance"
+  type        = string
+  default     = ""
 }
