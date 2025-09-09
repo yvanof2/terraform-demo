0174b1ab72c9cdb9b1e30da34236ee070838be15
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 02:05:42 -0400

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,13 +1,14 @@
-# Create the ALB
+# ----------------------
+# ALB
+# ----------------------
 resource "aws_lb" "this" {
   name               = "demo-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [var.subnet_id]
   security_groups    = [var.security_group_id]
+  subnets            = [var.subnet_id]   # only 1 subnet since 1AZ
 }
 
-# Create the Target Group
 resource "aws_lb_target_group" "this" {
   name     = "demo-tg"
   port     = 80
@@ -15,22 +16,16 @@ resource "aws_lb_target_group" "this" {
   vpc_id   = var.vpc_id
 }
 
-# Attach instances to Target Group
+# Single instance attachment
 resource "aws_lb_target_group_attachment" "this" {
-  for_each         = toset(var.instance_ids)
   target_group_arn = aws_lb_target_group.this.arn
-  target_id        = each.value
+  target_id        = var.instance_ids[0]   # first EC2 instance
   port             = 80
 }
 
-# Create a Listener
-resource "aws_lb_listener" "this" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.this.arn
-  }
+# ----------------------
+# Outputs
+# ----------------------
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
 }
