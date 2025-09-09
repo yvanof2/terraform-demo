16a59a516574270894a05bf8f797b8fecf726060
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 02:27:14 -0400

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,31 +1,4 @@
-variable "subnet_id" {
-  description = "Subnet ID where the RDS instance will be deployed"
-  type        = string
-}
-
-variable "db_name" {
-  description = "Database name"
-  type        = string
-}
-
-variable "username" {
-  description = "Database username"
-  type        = string
-}
-
-variable "password" {
-  description = "Database password"
-  type        = string
-  sensitive   = true
-}
-
-variable "instance_type" {
-  description = "RDS instance type"
-  type        = string
-  default     = "db.t3.micro"
-}
-
-variable "security_group_id" {
-  description = "Security group ID for RDS instance"
-  type        = string
+variable "subnet_ids" {
+  type = list(string)
+  description = "List of subnet IDs for the DB subnet group"
 }
