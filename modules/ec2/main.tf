f7466292df138a37f5680293d92687eb62d79448
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 00:48:35 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,9 +3,16 @@ resource "aws_instance" "this" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
 
+  vpc_security_group_ids = [var.security_group_id]
+
   user_data = var.user_data
 
   tags = {
-    Name = "DemoEC2"
+    Name = "Demo-EC2"
   }
+}
+
+# Output EC2 instance ID
+output "instance_id" {
+  value = aws_instance.this.id
 }
