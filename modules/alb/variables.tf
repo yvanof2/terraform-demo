14a87259a6b0c5dea6de5a997316314df2b48eb8
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 01:12:40 -0400

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,11 +1,14 @@
-variable "vpc_id" {
-  type = string
-}
-
 variable "subnet_id" {
-  type = string
+  description = "Subnet ID for the ALB"
+  type        = string
 }
 
-variable "target_id" {
-  type = string
+variable "security_group_id" {
+  description = "Security group ID for the ALB"
+  type        = string
+}
+
+variable "instance_ids" {
+  description = "List of EC2 instance IDs to register with ALB"
+  type        = list(string)
 }
