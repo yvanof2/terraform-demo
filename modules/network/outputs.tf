c726777d84d92258eb1cc53f8f0012bf3514d76c
yvanof2 <45571055+yvanof2@users.noreply.github.com>
2025-09-09 20:27:51 -0400

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -6,13 +6,12 @@ output "vpc_cidr" {
   value = aws_vpc.this.cidr_block
 }
 
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
+output "public_subnet_ids" {
+  value = [aws_subnet.public_az1.id, aws_subnet.public_az2.id]
 }
 
-output "private_subnet_id" {
-  value = aws_subnet.private.id
+output "private_subnet_ids" {
+  value = [aws_subnet.private_az1.id, aws_subnet.private_az2.id]
 }
 
 output "rds_sg_id" {
